c0f9a819e8fc2073eea8ee4a35bd704621555d84
Nadia <nadia.m.amir@gmail.com>
2021-03-09 15:25:53 +0000

    test for user at parametric endpoint

--- app.test.js
+++ app.test.js
@@ -19,13 +19,28 @@ describe('/api', ()=>{
                 .get('/api/topics')
                 .expect(200)
                 .then(({body})=>{
-                    expect(Array.isArray(body)).toBe(true)
-                    expect(body[0]).toMatchObject({
+                    expect(Array.isArray(body.topics)).toBe(true)
+                    expect(body.topics[0]).toMatchObject({
                         slug: expect.any(String),
                         description: expect.any(String)
                     })
                 })
             })
+            it("status:200, return specific username", ()=>{
+                return request(app)
+                .get('/api/users/lurker')
+                .expect(200)
+                .then(({body})=>{
+                    expect(Array.isArray(body.users)).toBe(true)
+                    expect(body.users[0]).toMatchObject({
+                        username: expect.any(String),
+                        avatar_url: expect.any(String),
+                        name: expect.any(String)
+                    })
+
+                })
+            })
+
         })
     })
 })
