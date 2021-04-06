e92d69d7874e4d7207b275e7c65ccd788d8a7679
Nadia <nadia.m.amir@gmail.com>
2021-04-06 16:43:33 +0100

    install/add cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =  require ('express');
 const { handleCustomError, handle400s, handle500s } = require('./Error-Handlers/index.js');
 const app = express()
 const apiRouter = require('./Routers/apiRouter.js')
-
+app.use(cors())
 
 
 app.use(express.json());
